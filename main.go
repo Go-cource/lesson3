4ecfbbdfbadad760c044ac2adaf587ea637f1926
Fisherman August <fshrmn.august@gmail.com>
2025-11-06 19:25:00 +0300

    format output

--- main.go
+++ main.go
@@ -3,21 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	const pi float64 = 3.14
-	const (
-		long  int = 12
-		width int = 12
-	)
-	const long2, width2 = 13, 14
-
-	const (
-		a = 1
-		b //b = ?
-		c //c = ?
-		d = 2
-		e //e = ?
-	)
-	
-	fmt.Println(a, b, c, d, e)
-
+	a := 1.0 / 3
+	fmt.Println(a)
+	fmt.Printf("%v\n", a)
+	fmt.Printf("%f\n", a)
+	fmt.Printf("%.2f\n", a)   //.2 - после запятой 2 числа
+	fmt.Printf("%4.2f\n", a) // 10 пробелов в начале
 }
